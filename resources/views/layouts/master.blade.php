4540706d2fe8604b05ef4c54ce4e4492d71a9591
Tay <tay.uorn@gmail.com>
2019-01-29 15:39:10 +0700

    delete folder migration

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,15 +16,9 @@
 
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 
-    {{-- <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-
     {{--cdn font awesome --}}
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-
-    {{-- <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
