6fc612125d8d1123ca605b6af1201867a4896095
Tay <tay.uorn@gmail.com>
2019-02-07 22:49:18 +0700

    update new project

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,50 @@
 */
 
 Route::get('/', function () {
+
     return view('welcome');
 });
+
+
+Route::get('/home',[
+
+    'uses' => 'Home\HomeController@index',
+    'as' => 'home'
+]);
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/home/farmer_store',[
+
+    'uses' => 'FarmerStore\FarmerStoreController@index',
+    'as' => 'home.farmer_store'
+]);
+
+Route::get('/create_admin', [
+    'uses' => 'CreateAdminController@load_form'
+]);
+
+Route::get('blade', function () {
+    return view('child');
+});
+Auth::routes();
+
+Route::post('/write_db',[
+
+    'uses' => 'CreateAdminController@write_db',
+    'as' => 'insert_data'
+
+]);
+
+
+Route::resource('farmerpost', 'FarmerPost\PostController');
+
+
+Route::post('/create/user',[
+
+    'uses' => 'User\UserController@store',
+    'as' => 'user.create'
+]);
